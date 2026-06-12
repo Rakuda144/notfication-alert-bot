54b704b109128a60b2895c325751c626769855cb
Rakuda144 <cheesecakety69@gmail.com>
2026-06-12 11:54:28 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,14 +2,16 @@ import requests
 
 url = "https://tenders.ongc.co.in/web/tendersweb/home?p_p_id=com_ongc_tender_OngcTenderWebPortlet_INSTANCE_oajq&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&_com_ongc_tender_OngcTenderWebPortlet_INSTANCE_oajq_javax.portlet.action=tender-currentNIT"
 
-response = requests.get(
+r = requests.get(
     url,
     headers={
         "User-Agent": "Mozilla/5.0"
-    },
-    timeout=30
+    }
 )
 
-print("Status:", response.status_code)
-print("Length:", len(response.text))
-print(response.text[:500])
+print("Status:", r.status_code)
+
+with open("response.html", "w", encoding="utf-8") as f:
+    f.write(r.text)
+
+print(r.text[:2000])
