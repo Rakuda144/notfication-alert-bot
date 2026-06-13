9c445be0fb951e1b4565d15d1b4c9974050eef1f
Rakuda144 <cheesecakety69@gmail.com>
2026-06-13 11:46:14 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,18 @@
-print("BOT STARTED SUCCESSFULLY")
+import requests
+
+URL = "https://assamtenders.gov.in/nicgep/app"
+
+print("BOT STARTED")
+
+try:
+    response = requests.get(
+        URL,
+        headers={"User-Agent": "Mozilla/5.0"},
+        timeout=30
+    )
+
+    print("HTTP STATUS:", response.status_code)
+    print("PAGE LENGTH:", len(response.text))
+
+except Exception as e:
+    print("ERROR:", e)
